30e45fba2984d387a12d9290b8e0dcb579e5f458
Joseph Hallett <josephhallett@gmail.com>
2016-02-24 19:09:02 +0000

    added location data to image

--- app/src/main/java/uk/ac/ed/inf/secureprogramming/MainActivity.java
+++ app/src/main/java/uk/ac/ed/inf/secureprogramming/MainActivity.java
@@ -1,11 +1,19 @@
 package uk.ac.ed.inf.secureprogramming;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
@@ -32,20 +40,20 @@ import java.util.Scanner;
  * Code running as part of the apps main screen.
  */
 public class MainActivity extends AppCompatActivity {
-    /**
-     * File referencing the current photo
-     * <p/>
+    /** File referencing the current photo
+     *
      * Null when no-photo currently exists.
      */
     private File currentPhoto = null;
 
-    /**
-     * Address and protocol of the server to upload to
-     */
+    /** Address and protocol of the server to upload to */
     String serverAddr = "http://infr11098.space";
 
-    /**
-     * Highly secure username and password
+    /** Handles Location callbacks */
+    LocationManager locationManager = null;
+    LocationListener locationListener = null;
+
+    /** A highly secure username and password
      */
     final static String username = "user123";
     final static String password = "dummy";
@@ -58,10 +66,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        this.locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Log.d(TAG, "Enabled provider: " + provider);
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d(TAG, "Disabled provider: " + provider);
+            }
+        };
     }
 
     /** Callback for when the user wants to take a picture */
     public void onTakePicture(View view) {
+        // Start tracking the user's location
+        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            if (this.locationManager != null && this.locationListener != null) {
+                //this.locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this.locationListener);
+            }
+        }
+
         this.dispatchTakePictureIntent();
     }
 
@@ -98,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),
                         "Failed to take picture",
                         Toast.LENGTH_SHORT).show();
-                Log.w(TAG, "error creating image file: "+ex);
+                Log.w(TAG, "error creating image file: " + ex);
             }
             // Continue only if the File was created
             if (photoFile != null) {
@@ -122,19 +158,38 @@ public class MainActivity extends AppCompatActivity {
     private void uploadPhoto(final File photo) {
         final EditText serverAddrText = (EditText) findViewById(R.id.serverAddr);
         serverAddr = serverAddrText.getText().toString();
+
+        tagPhoto(photo);
+
         new SendServerPhotoTask().execute(photo);
+    }
+
+    /** Tags the photo with GPS information if available */
+    private void tagPhoto(final File photo) {
+        try {
+            if (this.locationManager != null) {
+                final Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                final String path = photo.getAbsolutePath();
+                final ExifInterface exif = new ExifInterface(path);
+                exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, String.valueOf(location.getLatitude()));
+                exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, String.valueOf(location.getLongitude()));
+                exif.saveAttributes();
+            }
+        }
+        catch (SecurityException ex) {}
+        catch (IOException ex) { Log.d(TAG, "Something went wrong when tagging: "+ex); }
     }
 
     /** Task to perform the upload to the server */
     private class SendServerPhotoTask extends AsyncTask<File, Void, String> {
         protected String doInBackground(File... files) {
             for (File file : files) {
-                Log.d(TAG, "Uploading: "+file.getAbsolutePath());
+                Log.d(TAG, "Uploading: " + file.getAbsolutePath());
                 final URL url;
                 HttpURLConnection conn = null;
 
                 try {
-                    url = new URL(serverAddr +"/upload");
+                    url = new URL(serverAddr + "upload");
                     conn = (HttpURLConnection) url.openConnection();
                     conn.setDoOutput(true);
                     conn.setRequestProperty("Content-Type", "image/jpeg");
@@ -149,17 +204,17 @@ public class MainActivity extends AppCompatActivity {
 
                     Log.d(TAG, "Uploading photo");
                     final byte[] buffer = new byte[4096];
-                    while (true)
-                    {
+                    while (true) {
                         final int length = photo.read(buffer);
                         if (length < 0) break;
-                        Log.d(TAG, "Uploaded "+length+" bytes");
+                        Log.d(TAG, "Uploaded " + length + " bytes");
                         out.write(buffer);
                     }
                     try {
                         photo.close();
                     } catch (IOException ex) {
-                        Log.w(TAG, "Couldn't close photo");}
+                        Log.w(TAG, "Couldn't close photo");
+                    }
                     Log.d(TAG, "Finished uploading photo");
 
                     final Scanner resp = new Scanner(
@@ -182,6 +237,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         protected void onPostExecute(String result) {
+            try {
+                // locationManager.removeUpdates(locationListener);
+                Log.d(TAG, "Stopped tracking location");
+            }
+            catch (SecurityException ex) {}
+
             final TextView view = (TextView) findViewById(R.id.resultView);
             view.setText(result);
         }
